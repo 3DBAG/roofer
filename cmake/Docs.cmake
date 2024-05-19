b146c7f9ee74843ae42edd84ff0a86526347108e
Balázs Dukai <balazs.dukai@gmail.com>
2024-05-20 00:46:48 +0200

    Run doxygen directly instead through CMake
    Because running through CMake requires a full CMake configuration process which is unnecessary

--- cmake/Docs.cmake
+++
@@ -1,9 +0,0 @@
-find_package(Doxygen)
-
-if (DOXYGEN_FOUND)
-    add_custom_target(docs
-        COMMAND ${DOXYGEN_EXECUTABLE}
-        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/docs
-        COMMENT "Generating HTML documentation with Doxygen"
-    )
-endif()
