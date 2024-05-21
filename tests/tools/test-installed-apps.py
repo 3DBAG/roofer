6e77ecebf64fb8209847de5d3dc828f007899a60
Balázs Dukai <balazs.dukai@gmail.com>
2024-05-21 12:38:27 +0200

    Probably need crop.exe on Windows

--- tests/tools/test-installed-apps.py
+++ tests/tools/test-installed-apps.py
@@ -1,3 +1,4 @@
+import platform
 import subprocess
 from pathlib import Path
 
@@ -10,31 +11,49 @@ def print_error_message(output: subprocess.CompletedProcess):
 
 @pytest.fixture()
 def dir_tests() -> Path:
-    """Absolute path to the tests directory"""
+    """Absolute path to the tests directory."""
     return Path(__file__).parent.parent.absolute()
 
 
-def test_crop_version():
+@pytest.fixture()
+def crop_exe() -> str:
+    """Crop executable, OS-dependent."""
+    if platform.system() == 'Windows':
+        return 'crop.exe'
+    else:
+        return 'crop'
+
+
+@pytest.fixture()
+def reconstruct_exe() -> str:
+    """Reconstruct executable, OS-dependent."""
+    if platform.system() == 'Windows':
+        return 'reconstruct.exe'
+    else:
+        return 'reconstruct'
+
+
+def test_crop_version(crop_exe):
     """Can we print the crop exe version?"""
-    output = subprocess.run(["crop", "--version"], capture_output=True)
+    output = subprocess.run([crop_exe, "--version"], capture_output=True)
     assert output.returncode == 0, print_error_message(output)
 
 
-def test_crop_wippolder(dir_tests):
+def test_crop_wippolder(crop_exe, dir_tests):
     """Can we run crop on the wippolder data?"""
     path_config = dir_tests / "config" / "crop-wippolder.toml"
-    output = subprocess.run(["crop", "--config", path_config], capture_output=True)
+    output = subprocess.run([crop_exe, "--config", path_config], capture_output=True)
     assert output.returncode == 0, print_error_message(output)
 
 
-def test_reconstruct_version():
+def test_reconstruct_version(reconstruct_exe):
     """Can we print the reconstruct exe version?"""
-    output = subprocess.run(["reconstruct", "--version"], capture_output=True)
+    output = subprocess.run([reconstruct_exe, "--version"], capture_output=True)
     assert output.returncode == 0, print_error_message(output)
 
 
-def test_reconstruct_wippolder(dir_tests):
+def test_reconstruct_wippolder(reconstruct_exe, dir_tests):
     """Can we run reconstruct on the wippolder data?"""
     path_config = dir_tests / "config" / "reconstruct-wippolder.toml"
-    output = subprocess.run(["reconstruct", "--config", path_config], capture_output=True)
+    output = subprocess.run([reconstruct_exe, "--config", path_config], capture_output=True)
     assert output.returncode == 0, print_error_message(output)
