f9f98df12c5f41b71dda82f01d749c66cdf7deca
Ivan Paden <i.paden@tudelft.nl>
2024-06-17 11:20:34 +0200

    Fix tests for win

--- tests/tools/test_apps.py
+++ tests/tools/test_apps.py
@@ -59,8 +59,8 @@ class TestBindings:
 
     def test_python_bindings(self):
         """Can we run reconstruct with python bindings on the wippolder data?"""
-        so_path = os.path.join(os.path.dirname(__file__), '../../build/rooferpy')
-        path_script = os.path.join(os.path.dirname(__file__), '../../rooferpy/example_rooferpy.py')
+        so_path = os.path.normpath(os.path.join(os.path.dirname(__file__), '../../build/rooferpy'))
+        path_script = os.path.normpath(os.path.join(os.path.dirname(__file__), '../../rooferpy/example_rooferpy.py'))
 
         env = os.environ.copy()
         env['PYTHONPATH'] = so_path
@@ -68,6 +68,6 @@ class TestBindings:
         print(f"Running script: {path_script}")
         print(f"Using PYTHONPATH: {env['PYTHONPATH']}")
 
-        result = subprocess.run(['python3', path_script], capture_output=True, text=True, env=env, check=True)
+        result = subprocess.run([sys.executable, path_script], capture_output=True, text=True, env=env, check=True)
         print(f"stdout: {result.stdout}")
         print(f"stderr: {result.stderr}")
