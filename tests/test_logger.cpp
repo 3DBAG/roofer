577fc559f6e5f3702b0968f79d2fd63cf86952ca
Balázs Dukai <balazs.dukai@gmail.com>
2024-06-28 11:35:30 +0200

    Add logger unit test

---
+++ tests/test_logger.cpp
@@ -0,0 +1,13 @@
+#include <roofer/logger/logger.h>
+
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("logger") {
+  auto& logger = roofer::logger::Logger::get_logger();
+  logger.set_level(roofer::logger::LogLevel::trace);
+  logger.trace("trace");
+  logger.debug("debug");
+  logger.info("info");
+  logger.error("error");
+  logger.critical("critical");
+}
