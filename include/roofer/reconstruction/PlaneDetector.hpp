f01b82958249603d1c7e6fe7407d190ad982e667
Ylannl <ravi@ylan.nl>
2024-11-27 11:33:10 +0100

    only do slope and azimuth for lod22

--- include/roofer/reconstruction/PlaneDetector.hpp
+++ include/roofer/reconstruction/PlaneDetector.hpp
@@ -37,7 +37,7 @@ namespace roofer::reconstruction {
     float metrics_is_horizontal_threshold = 0.995;
     float metrics_probability_ransac = 0.05;
     float metrics_cluster_epsilon_ransac = 0.3;
-    float metrics_is_wall_threshold = 0.09;
+    float metrics_is_wall_threshold = 0.3;
     int n_refit = 5;
     bool use_ransac = false;
     // float roof_percentile=0.5;
